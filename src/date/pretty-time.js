b39ae467f79103171ca85c0770e832e6277df7b6
ssk101 <steelskysoftware@gmail.com>
2020-12-11 17:37:01 +0100

    pretty-time

---
+++ src/date/pretty-time.js
@@ -0,0 +1,21 @@
+export function PrettyDate(v) {
+  const pad = (v) => String(v).padStart(2, 0)
+
+  const date = new Date(v)
+
+  return [
+    date.getFullYear(),
+    '/',
+    date.getMonth() + 1,
+    '/',
+    date.getDate(),
+    ' ',
+    pad(date.getHours()),
+    ':',
+    pad(date.getMinutes()),
+    ':'
+    pad(date.getSeconds()),
+    '.'
+    pad(date.getMilliseconds()),
+  ].join('')
+}
