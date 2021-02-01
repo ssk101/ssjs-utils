01fcdd9340404d029937a70187b958ba20a131bb
ssk101 <steelskysoftware@gmail.com>
2021-02-01 11:34:57 +0100

    v1.14.1

--- dist/jsutils.esm.js
+++ dist/jsutils.esm.js
@@ -357,11 +357,39 @@ function randomChars(length = 5, chars) {
   return ret.join('')
 }
 
-function base36(min, max) {
-  return Math
-    .floor(Math.random() * (max - min + 1) + min)
-    .toString(36)
-    .toUpperCase()
-}
+const base36 = {
+  random(min, max) {
+    return Math
+      .floor(Math.random() * (max - min + 1) + min)
+      .toString(36)
+      .toUpperCase()
+  }
+};
 
-export { abbreviationCase, base36, blendHex, camelCase, camelKeys, camelize, clamp, hexToRGB, humanCase, imgFromBlob, imgFromBuffer, imgToDataUri, jsonToCSS, kebabCase, objectToStyle, objectWithPath, prettyDate, prettyTime, probability, randomChars, randomInt, randomItems, resizeWithAspectRatio, sentenceCase, snakeCase, snakeKeys, validURL, varsToHex };
+const NUMBERS = [...Array(10)].map((_, y) => String.fromCharCode(y + 48));
+const LOWER = [...Array(26)].map((_, y) => String.fromCharCode(y + 97));
+const UPPER = [...Array(26)].map((_, y) => String.fromCharCode(y + 65));
+const DIGITS = NUMBERS.concat(LOWER).concat(UPPER);
+
+const base62 = {
+  encode(int) {
+    if(int === 0) {
+      return 0
+    }
+
+    let s = [];
+    while (int > 0) {
+      s = [DIGITS[int % 62], ...s];
+      int = Math.floor(int / 62);
+    }
+
+    return s.join('')
+  },
+  decode(str) {
+    return str.split('').reverse().reduce((acc, cur, i) => {
+      return acc + (DIGITS.indexOf(cur) * (62 ** i))
+    }, 0)
+  }
+};
+
+export { abbreviationCase, base36, base62, blendHex, camelCase, camelKeys, camelize, clamp, hexToRGB, humanCase, imgFromBlob, imgFromBuffer, imgToDataUri, jsonToCSS, kebabCase, objectToStyle, objectWithPath, prettyDate, prettyTime, probability, randomChars, randomInt, randomItems, resizeWithAspectRatio, sentenceCase, snakeCase, snakeKeys, validURL, varsToHex };
--- src/uid/base36.js
+++ src/uid/base36.js
@@ -1,6 +1,8 @@
-export function base36(min, max) {
-  return Math
-    .floor(Math.random() * (max - min + 1) + min)
-    .toString(36)
-    .toUpperCase()
+export const base36 = {
+  random(min, max) {
+    return Math
+      .floor(Math.random() * (max - min + 1) + min)
+      .toString(36)
+      .toUpperCase()
+  }
 }
